70f409098c13fe557d45d983ce460c21d2d0515d
henry florence <henry@hflorence.com>
2018-01-14 15:27:27 +0000

    got random working

--- Nixie_Dice.ino
+++ Nixie_Dice.ino
@@ -5,19 +5,28 @@
 */
 
 void setup() {
-  //randomSeed(analogRead(0));
+  randomSeed(analogRead(2));
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
   pinMode(13, OUTPUT);
 
-  int throwLength = random(10, 20);
-  for(int i=0; i < throwLength; i++) {
+  for(int i=0; i < 10; i++) {
     digitalWrite(2, HIGH && (i % 10 & 1));
     digitalWrite(3, HIGH && (i % 10 & 2));
     digitalWrite(4, HIGH && (i % 10 & 4));
     digitalWrite(5, HIGH && (i % 10 & 8));
+    delay(300);
+  }
+  
+  int throwLength = random(1, 12);
+  for(int i=0; i < throwLength; i++) {
+    int num = random(0,10);
+    digitalWrite(2, HIGH && (num % 10 & 1));
+    digitalWrite(3, HIGH && (num % 10 & 2));
+    digitalWrite(4, HIGH && (num % 10 & 4));
+    digitalWrite(5, HIGH && (num % 10 & 8));
     delay(300);
   }
   digitalWrite(13, LOW);
